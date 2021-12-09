e0e0f7dfda2e5b2a13bc1a9e07c6d5ff90775fce
Adrian Wilker <56568057+adrianwilker@users.noreply.github.com>
2021-12-09 15:20:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -17,17 +17,14 @@ changeSocialMediaLinks()
 
 function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${linksSocialMedia.github}`
-  // fetch pega o que a URL tem
-  // .then() é uma promess que vai pegar a resposta que ta no fetch
-  // arrow function é uma forma contraída de função. função anonima (sem nome): (argumento)=>{}
   fetch(url)
-    .then(response => response.json()) // o que foi recebido é transformado em JSON
+    .then(response => response.json())
     .then(data => {
       userName.textContent = data.name
       userBio.textContent = data.bio
       userLink.href = data.html_url
       userImage.src = data.avatar_url
       userLogin.textContent = data.login
-    }) // data armazena a resposta só que em JSON
+    })
 }
 getGitHubProfileInfos()
